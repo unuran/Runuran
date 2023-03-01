632eacc3a47ec4946a92eb418653be6149b0dc0b
jleydold <josef.leydold@wu.ac.at>
2023-03-01 11:47:57 +0100

    Updated test suite
       - test routines are now all defined in new file'test_routines.R'
       - use new environment 'unur.envir' for storing counters
       - added optional arguments 'rfunc' and 'pfunc' for test routine
         unur.test.cont()
       - added optional arguments 'rfunc', 'dfunc', and 'pv'
         for test routine unur.test.discr()
       - added tests for Runuran S4 distributions objects

---
+++ Runuran/tests/test_methods.R
@@ -0,0 +1,42 @@
+#############################################################################
+##                                                                         ##
+##   Tests for methods                                                     ##
+##                                                                         ##
+#############################################################################
+
+## --- Load test routines and test parameters -------------------------------
+
+source("test_routines.R")
+
+## --- CONT: Chi^2 goodness-of-fit test -------------------------------------
+
+## TDR (Transformed Density Rejection)
+tdr.norm <- function (n) {
+        pdf <- function (x) { exp(-0.5*x^2) }
+        dpdf <- function (x) { -x*exp(-0.5*x^2) }
+        dist <- new("unuran.cont", pdf=pdf, dpdf=dpdf)
+        gen <- unuran.new(dist, "tdr")
+        unuran.sample(gen,n)
+}
+unur.test.cont("normS4", rfunc=tdr.norm, pfunc=pnorm)
+
+## --- DISCR: Chi^2 goodness-of-fit test ------------------------------------
+
+## DGT (Discrete Guide Table method)
+size <- 100
+prob <- 0.3
+binom.pmf <- function (x) { dbinom(x, size, prob) }
+binom.probs <- dbinom(0:size, size, prob)
+dgt.binom <- function (n,lb=0,ub=size) {
+        dist <- new("unuran.discr", pv=binom.probs)
+        gen <- unuran.new(dist, "dgt")
+        unuran.sample(gen,n)
+}
+unur.test.discr("binomS4", rfunc=dgt.binom, dfunc=binom.pmf, domain=c(0,size))
+unur.test.discr("binomS4", rfunc=dgt.binom, pv=binom.probs, domain=c(0,size))
+
+## -- Print statistics ------------------------------------------------------
+
+unur.test.statistic()
+
+## -- End -------------------------------------------------------------------
