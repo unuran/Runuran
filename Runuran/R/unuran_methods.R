bde041125cc4cd3457b5a28e3c65e29e1a02d302
jleydold <josef.leydold@wu.ac.at>
2023-03-01 14:50:15 +0100

    Added check for missing arguments

--- Runuran/R/unuran_methods.R
+++ Runuran/R/unuran_methods.R
@@ -28,8 +28,21 @@
 ##
 
 urtdr <- function (n, pdf, dpdf=NULL, lb=-Inf, ub=Inf, islog=FALSE) {
+        ## the probability density function is obligatory and must be a function
+        if (missing(pdf))
+                stop ("argument 'pdf' required")
+        if (! is.function(pdf))
+                stop ("argument 'pdf' must be of class 'function'")
+        ## we also need the derivative of the PDF
+        if (missing(dpdf))
+                stop ("argument 'dpdf' required")
+        if (! is.function(dpdf))
+                stop ("argument 'dpdf' must be of class 'function'")
+        ## S4 class for discrete distribution
         dist <- new("unuran.cont", pdf=pdf, dpdf=dpdf, lb=lb, ub=ub, islog=islog)
+        ## create UNU.RAN object
         unr <- unuran.new(dist, "tdr")
+        ## draw sample
         unuran.sample(unr,n)
 }
 
@@ -46,10 +59,20 @@ urtdr <- function (n, pdf, dpdf=NULL, lb=-Inf, ub=Inf, islog=FALSE) {
 ## Generate discrete random variates from a given probability vector
 ## using the Guide-Table Method for discrete inversion
 ##
+## Remark: we do not pass the domain to UNU.RAN
+##
 
 urdgt <- function (n, probvector, from = 0, by = 1) {
+        ## the probability vector is obligatory
+        if (missing(probvector))
+                stop ("argument 'probvector' required")
+        if (!is.numeric(probvector))
+                stop ("argument 'probvector' must be of class 'numeric'")
+        ## S4 class for discrete distribution
         distr <- new("unuran.discr",pv=probvector)
+        ## create UNU.RAN object
         unr <- new("unuran", distr, "DGT")
+        ## draw sample
         if (from==0 && by==1)
                 unuran.sample(unr,n)
         else
@@ -63,10 +86,20 @@ urdgt <- function (n, probvector, from = 0, by = 1) {
 ## Generate discrete random variates from a given probability vector
 ## using the Alias-Urn Method
 ##
+## Remark: we do not pass the domain to UNU.RAN
+##
 
 urdau <- function (n, probvector, from = 0, by = 1) {
+        ## the probability vector is obligatory
+        if (missing(probvector))
+                stop ("argument 'probvector' required")
+        if (!is.numeric(probvector))
+                stop ("argument 'probvector' must be of class 'numeric'")
+        ## S4 class for discrete distribution
         distr <- new("unuran.discr",pv=probvector)
+        ## create UNU.RAN object
         unr <- new("unuran", distr, "DAU")
+        ## draw sample
         if (from==0 && by==1)
                 unuran.sample(unr,n)
         else
