d1aff370f7c931f4d2445a2575111cca92378ce1
jleydold <josef.leydold@wu.ac.at>
2023-03-02 10:58:21 +0100

    New functions *.new()
       - added new function srou.new()
       - added new function itdr.new()
       - added new function vnrou.new()
       - added new function dari.new()
       - added new function pinv.new()
    
       - added man pages and tests for these new functions
       - update man pages

--- Runuran/R/universal.R
+++ Runuran/R/universal.R
@@ -41,21 +41,21 @@ numerical.derivative <- function (x, func, lb=-Inf, ub=Inf, xmin=1, delta=1.e-7)
 
 ## -- ARS: Adaptive Rejection Sampling (TDR with T=log) ---------------------
 ##
-## Type: Acceptance-Rejection
+## Type: Rejection
 ##
 ## Generate continuous random variates from a given PDF
 ##
 
 ars.new <- function (logpdf, dlogpdf=NULL, lb=-Inf, ub=Inf, ...) {
-        ## the probability density function is obligatory and must be a function
 
-        if (missing(logpdf))
-                stop ("argument 'logpdf' required")
-        if (! is.function(logpdf))
-                stop ("argument 'logpdf' must be of class 'function'")
+        ## check arguments
+        if (missing(logpdf) || !is.function(logpdf))
+                stop ("argument 'logpdf' missing or invalid")
+
+        ## internal version of logPDF
         f <- function(x) logpdf(x, ...) 
-
-        ## we also need the derivative of the PDF
+        
+        ## derivative of the PDF
         if (is.null(dlogpdf)) {
                 ## use numerical derivative
                 df <- function(x) {
@@ -64,7 +64,7 @@ ars.new <- function (logpdf, dlogpdf=NULL, lb=-Inf, ub=Inf, ...) {
         }
         else {
                 if (! is.function(dlogpdf) )
-                        stop ("argument 'dlogpdf' must be of class 'function'")
+                        stop ("argument 'dlogpdf' invalid")
                 else df <- function(x) dlogpdf(x,...)
 	}	
 
@@ -73,23 +73,131 @@ ars.new <- function (logpdf, dlogpdf=NULL, lb=-Inf, ub=Inf, ...) {
 
         ## create and return UNU.RAN object
         unuran.new(dist, "ars")
+} 
+
+
+## -- ITDR: Inverse Transformed Density Rejection ---------------------------
+##
+## Type: Rejection
+##
+## Generate continuous random variates from a given PDF
+##
+
+itdr.new <- function (pdf, dpdf, lb=0, ub=Inf, islog=FALSE, pole, ...) {
+
+        ## check arguments 
+        if (missing(pdf) || !is.function(pdf))
+                stop ("argument 'pdf' missing or invalid")
+        if (missing(dpdf) || !is.function(dpdf))
+                stop ("argument 'dpdf' missing or invalid")
+        if (missing(pole) || !is.numeric(pole)) 
+                stop ("argument 'pole' missing or invalid")
+
+        ## internal versions of PDF and its derivative
+        f <- function(x) pdf(x, ...) 
+        df <- function(x) dpdf(x, ...) 
+        
+        ## S4 class for continuous distribution
+        dist <- new("unuran.cont", pdf=f, dpdf=df, lb=lb, ub=ub, islog=islog, mode=pole)
+
+        ## create and return UNU.RAN object
+        unuran.new(dist, "itdr")
+}
+
+
+## -- PINV:  Polynomial interpolation based INVersion -----------------------
+##
+## Type: Inversion
+##
+## Generate continuous random variates from a given PDF or CDF
+##
+
+pinv.new <- function (pdf, cdf, lb=-Inf, ub=Inf, islog=FALSE, center=0, uresolution=1.e-10, ...) {
+
+        ## check arguments
+        if (missing(pdf) && missing(cdf))
+                stop ("argument 'pdf' or 'cdf' required")
+        if (!is.numeric(center))
+                stop ("argument 'center' invalid")
+        if (!is.numeric(uresolution))
+                stop ("argument 'uresolution' invalid")
+
+        ## use PDF or CDF ?
+        usefunc <- if (missing(pdf)) "usecdf;" else "usepdf;"
+                
+        ## create internal version of PDF and CDF
+        if (!missing(pdf)) {
+                ## PDF given
+                if (!is.function(pdf))
+                        stop ("argument 'pdf' must be of class 'function'")
+                PDF <- function(x) pdf(x, ...)
+        }
+        else {
+                PDF <- NULL;
+        }
+
+        if (!missing(cdf)) {
+                ## CDF given
+                if (!is.function(cdf))
+                        stop ("argument 'cdf' must be of class 'function'")
+                CDF <- function(x) cdf(x, ...)
+        }
+        else {
+                CDF <- NULL
+        }
+
+        ## S4 class for continuous distribution
+        dist <- new("unuran.cont", pdf=PDF, cdf=CDF, lb=lb, ub=ub, center=center, islog=islog)
+
+        ## create and return UNU.RAN object
+        method <- paste("pinv;",usefunc,"u_resolution=",uresolution, sep="")
+        return (unuran.new(dist,method))
+}
+
+
+## -- SROU: Simple Ratio-Of-Uniforms Method ---------------------------------
+##
+## Type: Rejection
+##
+## Generate continuous random variates from a given PDF
+##
+
+srou.new <- function (pdf, lb=-Inf, ub=Inf, islog=FALSE, mode=NA, area=NA, r=1, ...) {
+
+        ## check arguments
+        if (missing(pdf) || !is.function(pdf))
+                stop ("argument 'pdf' missing or invalid")
+        if (missing(mode) || !is.numeric(mode))
+                stop ("argument 'mode' missing or invalid")
+        if (missing(area) || !is.numeric(area))
+                stop ("argument 'area' missing or invalid")
+
+        ## internal version of PDF
+        f <- function(x) pdf(x, ...) 
+
+        ## S4 class for continuous distribution
+        dist <- new("unuran.cont", pdf=f, lb=lb, ub=ub, islog=islog, mode=mode, area=area)
+
+        ## create and return UNU.RAN object
+        method <- paste("srou; r=",r, sep="")
+        unuran.new(dist, method)
 }
 
 
 ## -- TDR: Transformed Density Rejection ------------------------------------
 ##
-## Type: Acceptance-Rejection
+## Type: Rejection
 ##
 ## Generate continuous random variates from a given PDF
 ##
 
 tdr.new <- function (pdf, dpdf=NULL, lb=-Inf, ub=Inf, islog=FALSE, ...) {
-        ## the probability density function is obligatory and must be a function
 
-        if (missing(pdf))
-                stop ("argument 'pdf' required")
-        if (! is.function(pdf))
-                stop ("argument 'pdf' must be of class 'function'")
+        ## check arguments
+        if (missing(pdf) || !is.function(pdf))
+                stop ("argument 'pdf' missing or invalid")
+
+        ## internal version of PDF
         f <- function(x) pdf(x, ...) 
 
         ## we also need the derivative of the PDF
@@ -101,7 +209,7 @@ tdr.new <- function (pdf, dpdf=NULL, lb=-Inf, ub=Inf, islog=FALSE, ...) {
         }
         else {
                 if (! is.function(dpdf) )
-                        stop ("argument 'dpdf' must be of class 'function'")
+                        stop ("argument 'dpdf' invalid")
                 else df <- function(x) dpdf(x,...)
 	}	
 
@@ -119,6 +227,31 @@ tdr.new <- function (pdf, dpdf=NULL, lb=-Inf, ub=Inf, islog=FALSE, ...) {
 ##                                                                          #
 #############################################################################
 
+## -- DARI: Discrete Automatic Rejection Inversion --------------------------
+##
+## Type: Rejection
+##
+## Generate discrete random variates from a given probability vector
+## using Discrete Automatic Rejection Inversion.
+##
+
+dari.new <- function (pmf, lb=0, ub=Inf, mode=NA, sum=1, ...) {
+
+        ## check arguemngts
+        if (missing(pmf) || !is.function(pmf))
+                stop ("argument 'pmf' missing or invalid")
+
+        ## internal version of PMF
+        f <- function(x) pmf(x, ...)
+        
+        ## S4 class for discrete distribution
+        distr <- new("unuran.discr",pmf=f,lb=lb,ub=ub,mode=mode,sum=sum)
+
+        ## create and return UNU.RAN object
+        new("unuran", distr, "DARI")
+}
+
+
 ## -- DAU: Alias-Urn Method ------------------------------------------------
 ##
 ## Type: Patchwork
@@ -130,11 +263,10 @@ tdr.new <- function (pdf, dpdf=NULL, lb=-Inf, ub=Inf, islog=FALSE, ...) {
 ##
 
 dau.new <- function (pv, from=1) {
-        ## the probability vector is obligatory
-        if (missing(pv))
-                stop ("argument 'pv' required")
-        if (!is.numeric(pv))
-                stop ("argument 'pv' must be of class 'numeric'")
+
+        ## check arguments
+        if (missing(pv) || !is.numeric(pv))
+                stop ("argument 'pv' missing or invalid")
 
         ## S4 class for discrete distribution
         distr <- new("unuran.discr",pv=pv,lb=from)
@@ -154,11 +286,10 @@ dau.new <- function (pv, from=1) {
 ##
 
 dgt.new <- function (pv, from=1) {
-        ## the probability vector is obligatory
-        if (missing(pv))
-                stop ("argument 'pv' required")
-        if (!is.numeric(pv))
-                stop ("argument 'pv' must be of class 'numeric'")
+
+        ## check arguments
+        if (missing(pv) || !is.numeric(pv))
+                stop ("argument 'pv' missing or invalid")
 
         ## S4 class for discrete distribution
         distr <- new("unuran.discr",pv=pv,lb=from)
@@ -182,12 +313,13 @@ dgt.new <- function (pv, from=1) {
 ## Generate continuous random variates from a given PDF
 ##
 
-hitro.new <- function (dim=1, pdf, mode=NULL, center=NULL, ll=NULL, ur=NULL, thinning=1, burnin=0, ...) {
-        ## the probability density function is obligatory and must be a function
-        if (missing(pdf))
-                stop ("argument 'pdf' required")
-        if (! is.function(pdf))
-                stop ("argument 'pdf' must be of class 'function'")
+hitro.new <- function (dim=1, pdf, ll=NULL, ur=NULL, mode=NULL, center=NULL, thinning=1, burnin=0, ...) {
+
+        ## check arguments
+        if (missing(pdf) || !is.function(pdf))
+                stop ("argument 'pdf' missing or invalid")
+
+        ## internal version of PDF
         f <- function(x) pdf(x, ...) 
 
         ## S4 class for continuous multivariate distribution
@@ -196,6 +328,29 @@ hitro.new <- function (dim=1, pdf, mode=NULL, center=NULL, ll=NULL, ur=NULL, thi
         ## create and return UNU.RAN object
         method <- paste("hitro;thinning=",thinning,";burnin=",burnin, sep="")
         unuran.new(dist, method)
+}
+
+## -- VNROU: Multivariate Naive Ratio-Of-Uniforms method --------------------
+##
+## Type: Rejection / Ratio-of-Uniforms
+##
+## Generate continuous random variates from a given PDF
+##
+
+vnrou.new <- function (dim=1, pdf, ll=NULL, ur=NULL, mode=NULL, center=NULL, ...) {
+
+        ## check arguments
+        if (missing(pdf) || !is.function(pdf))
+                stop ("argument 'pdf' missing or invalid")
+
+        ## internal version of PDF
+        f <- function(x) pdf(x, ...) 
+
+        ## S4 class for continuous multivariate distribution
+        dist <- new("unuran.cmv", dim=dim, pdf=f, mode=mode, center, ll=ll, ur=ur)
+
+        ## create and return UNU.RAN object
+        unuran.new(dist, "VNROU")
 }
 
 ## -- End -------------------------------------------------------------------
--- Runuran/tests/test_universal.R
+++ Runuran/tests/test_universal.R
@@ -43,6 +43,70 @@ unur.test.cont("ur.ars.norm.param", rfunc=ur.ars.norm.param, pfunc=pnorm)
 rm(ur.ars.norm.param)
 
 
+## --- ITDR (Inverse Transformed Density Rejection) -------------------------
+
+ur.itdr.exp <- function (n) {
+        pdf <- function (x) { exp(-x) }
+        dpdf <- function (x) { -exp(-x) }
+        gen <- itdr.new(pdf=pdf, dpdf=dpdf,lb=0,pole=0)
+        ur(gen,n)
+}
+unur.test.cont("ur.itdr.exp", rfunc=ur.itdr.exp, pfunc=pexp)
+rm(ur.itdr.exp)
+
+cdf.gamma <- function(x) { pgamma(x,0.5) }
+ur.itdr.gamma <- function (n) {
+        pdf <- function (x) { x^(-0.5)*exp(-x) }
+        dpdf <- function (x) { (-x^(-0.5) - 0.5*x^(-1.5))*exp(-x) }
+        gen <- itdr.new(pdf=pdf, dpdf=dpdf,lb=0,pole=0)
+        ur(gen,n)
+}
+unur.test.cont("ur.itdr.gamma", rfunc=ur.itdr.gamma, pfunc=cdf.gamma)
+rm(ur.itdr.gamma)
+rm(cdf.gamma)
+
+
+## --- PINV (Polynomial interpolation based INVersion) ----------------------
+
+ur.pinv.norm.pdf <- function (n) {
+        gen <- pinv.new(pdf=dnorm)
+        ur(gen,n)
+}
+unur.test.cont("ur.pinv.norm.pdf", rfunc=ur.pinv.norm.pdf, pfunc=pnorm)
+rm(ur.pinv.norm.pdf)
+
+ur.pinv.norm.cdf <- function (n) {
+        gen <- pinv.new(cdf=pnorm)
+        ur(gen,n)
+}
+unur.test.cont("ur.pinv.norm.cdf", rfunc=ur.pinv.norm.cdf, pfunc=pnorm)
+rm(ur.pinv.norm.cdf)
+
+ur.pinv.norm.pdfcdf <- function (n) {
+        gen <- pinv.new(pdf=dnorm,cdf=pnorm)
+        ur(gen,n)
+}
+unur.test.cont("ur.pinv.norm.pdfcdf", rfunc=ur.pinv.norm.pdfcdf, pfunc=pnorm)
+rm(ur.pinv.norm.pdfcdf)
+
+
+## --- SROU (Simple Ratio-Of-Uniforms Method) -------------------------------
+
+ur.srou.norm <- function (n) {
+        gen <- srou.new(pdf=dnorm, mode=0, area=1)
+        ur(gen,n)
+}
+unur.test.cont("ur.srou.norm", rfunc=ur.srou.norm, pfunc=pnorm)
+rm(ur.srou.norm)
+
+ur.srou.2.norm <- function (n) {
+        gen <- srou.new(pdf=dnorm, mode=0, area=1, r=2)
+        ur(gen,n)
+}
+unur.test.cont("ur.srou.2.norm", rfunc=ur.srou.2.norm, pfunc=pnorm)
+rm(ur.srou.2.norm)
+
+
 ## --- TDR (Transformed Density Rejection) ----------------------------------
 
 ur.tdr.norm <- function (n) {
@@ -109,6 +173,31 @@ rm(ur.tdr.t.R)
 ##                                                                          #
 #############################################################################
 
+## --- DARI (Discrete Automatic Rejection Inversion) ------------------------
+
+size <- 1000
+prob <- 0.2
+binom.pmf <- function (x) { dbinom(x, size, prob) }
+ur.dari.binom <- function (n,lb=0,ub=size) {
+        gen <- dari.new(pmf=binom.pmf,lb=0,ub=size)
+        ur(gen,n)
+}
+unur.test.discr("ur.dari.binom", rfunc=ur.dari.binom, dfunc=binom.pmf, domain=c(0,size))
+rm(ur.dari.binom)
+rm(size,prob,binom.pmf)
+
+size <- 100000
+prob <- 0.1
+geom.pmf <- function (x) { dgeom(x, prob) }
+ur.dari.geom <- function (n,lb=0,ub=size,mode=0) {
+        gen <- dari.new(pmf=geom.pmf,lb=0,ub=size)
+        ur(gen,n)
+}
+unur.test.discr("ur.dari.geom", rfunc=ur.dari.geom, dfunc=geom.pmf, domain=c(0,size))
+rm(ur.dari.geom)
+rm(size,prob,geom.pmf)
+
+
 ## --- DAU (Discrete Alias-Urn method) --------------------------------------
 
 size <- 100
@@ -158,6 +247,14 @@ ur.hitro.norm <- function (n) {
 }
 unur.test.cmv("ur.hitro.norm", rfunc=ur.hitro.norm, pfunc=pnorm)
 rm(ur.hitro.norm)
+
+ur.vnrou.norm <- function (n) {
+        pdf <- function (x) { exp(-0.5*sum(x^2)) }
+        gen <- vnrou.new(dim=2, pdf=pdf, mode=c(0,0))
+        ur(gen,n)
+}
+unur.test.cmv("ur.vnrou.norm", rfunc=ur.vnrou.norm, pfunc=pnorm)
+rm(ur.vnrou.norm)
 
 
 ## -- Print statistics ------------------------------------------------------
