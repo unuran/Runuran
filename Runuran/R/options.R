2869193bf4502ebae16ed6cefae062f014ab1882
jleydold <josef.leydold@wu.ac.at>
2023-03-20 22:57:31 +0100

    Add help page for Runuran.options() using roxygen2
       - used roxygen2 to generate help page for
         routine Runuran.options()

--- Runuran/R/options.R
+++ Runuran/R/options.R
@@ -64,136 +64,105 @@ unuran.error.level.default = "warning"
     error.level = .Runuran.options.set.error.level
 )
 
-### FIXME: help page for Runuran.options()
-### ==========================================================================
-###'
-###' Set or return options of rvgt library
-###' 
-### --------------------------------------------------------------------------
-###'
-###' @description
-###'
-###' Library \pkg{rvgt} has some parameters which (usually) affect the
-###' behavior of its functions. These can be set for the whole session
-###' via \code{rvgt.options}.
-###' 
-### --------------------------------------------------------------------------
-###'
-###' @details
-###'
-###' This function provides a tool to control the behavior of library
-###' \pkg{rvgt}. A list may be given as the only argument, or any
-###' number of arguments may be in the \code{name=value} form.
-###' If no arguments are specified then the function returns the
-###' current settings of all parameters. 
-###' If a single option name is given as character string, then its
-###' value is returned (or \code{NULL} if it does not exist).
-###' Option \emph{values} may be abbreviated.
-###'
-###' The currently used parameters in alphabetical order:
-###' \describe{
-###' \item{rngkind:}{
-###'    character or \code{NULL} that is used to set the
-###'    underlying uniform RNG for tests,
-###'    see \code{\link{RNGkind}} for possible values.
-###'    Notice, however, that this RNG is used \emph{only} if the
-###'    \code{seed} argument is set in a particular test.
-###' }
-###' \item{sessioninfo:}{
-###'    logical. If \code{TRUE} (the default), then package versions
-###'    are shown with \code{\link{rvgt.show}}.
-###' }
-###' \item{timing:}{
-###'    character string that controls the timing method for estimating
-###'    marginal running times. The following methods can be used:
-###'    \describe{
-###'    \item{\code{"cputime"}:}{
-###'       user and system CPU time from \code{\link{proc.time}} is used.
-###'       This method is less volatile on a busy machine
-###'       but has a coarser time resolution than the other methods.
-###'       (default)}
-###'    \item{\code{"elapsed"}:}{
-###'       the elapsed time from \code{\link{proc.time}} is used.}
-###'    \item{\code{"microbm"}:}{
-###'       function \code{\link[microbenchmark]{get_nanotime}} from package
-###'       \pkg{microbenchmark} is used.
-###'       Note that currently function
-###'       \code{\link[microbenchmark]{microbenchmark}} is not used.}
-###'    }
-###' }
-###' \item{uresolution:}{
-###'    numeric between \code{1e-5} and \code{1e-15}.
-###'    It is used whenever function \code{\link[Runuran]{pinv.new}}
-###'    from \pkg{Runuran} is called internally.
-###'    Default is \code{1e-12}.
-###' }
-###' \item{color.trans:}{
-###'    Debugging tool.
-###'    Character string that controls a possible transforming the
-###'    colors used in (some) \pkg{rvgt} plots. This may help to find
-###'    colors that are also suitable for B/W printers and color blind
-###'    people. 
-###'    Possible values are:
-###'    \describe{
-###'    \item{\code{"none"}:}{
-###'       No transformation is performed.  (default)}
-###'    \item{\code{"grey"}:}{
-###'       Transforms the given colors to the corresponding colors
-###'       with chroma removed (collapsed to zero) in HCL space.
-###'       See \code{\link[colorspace]{desaturate}} in the
-###'       \pkg{colorspace} package for more details.}
-###'    \item{\code{"deutan"}, \code{"protan"}, \code{"tritan"}:}{
-###'       Collapses red-green or green-blue color distinctions to
-###'       approximate the effect of the three forms of dichromacy:
-###'       protanopia and deuteranopia (red-green color blindness), and
-###'       tritanopia (green-blue color blindness).
-###'       See \code{\link[dichromat]{dichromat}} in the
-###'       \pkg{dichromat} package for more details.}
-###'    \item{\code{"DEBUG-R"}, \code{"DEBUG-G"}, \code{"DEBUG-B"}:}{
-###'       Turn all colors into plain red, green, and blue, respectively.
-###'       Only useful to detect colors that are not influenced by this
-###'       option.}
-###'    }
-###' }
-#### \item{DEBUG:}{
-####    Debugging switch. Must be named list.
-####    Possible values:
-####      list(legend.colors=TRUE)
-#### }
-###' }
-### 
-### --------------------------------------------------------------------------
-###'
-###' @author Josef Leydold \email{josef.leydold@@wu.ac.at}
-###'
-### --------------------------------------------------------------------------
-###' 
-###' @examples
-###' 
-###' oldval <- rvgt.options()
-###' rvgt.options(timing="elapsed")
-###' rvgt.options(oldval)
-###'
-### --------------------------------------------------------------------------
-###
-###  Arguments:
-###
-###' @param \dots
-###'        A list may be given as the only argument, or any number of
-###'        arguments may be in the \code{name=value} form, a character
-###'        string for the name of a parameter, or no argument at all
-###'        may be given.
-###'
-### --------------------------------------------------------------------------
-###'
-###' @return
-###'
-###' \code{rvgt.options} returns a list with the updated values of the
-###' parameters. If the argument list is not empty, the returned list
-###' is invisible. If a single character string is given, then the
-###' value of the corresponding parameter is returned (or \code{NULL}
-###' if the parameter is not used).
-###'
+## ==========================================================================
+##'
+##' Set or return options for Runuran library
+##' 
+## --------------------------------------------------------------------------
+##'
+##' @description
+##'
+##' Library \pkg{Runuran} has some parameters which (usually) affect the
+##' behavior of its functions. These can be set for the whole session
+##' via \code{Runuran.options}.
+##' 
+## --------------------------------------------------------------------------
+##'
+##' @details
+##'
+##' The function provides a tool to control the behavior of library
+##' \pkg{Runuran}. A list may be given as the only argument, or any
+##' number of arguments may be in the \code{name=value} form.
+##' If no arguments are specified then the function returns the
+##' current settings of all parameters. 
+##' If a single option name is given as character string, then its
+##' value is returned (or \code{NULL} if it does not exist).
+##' Option \emph{values} may be abbreviated.
+##'
+##' Currently used parameters in alphabetical order:
+##' \describe{
+##'   \item{error.level}{
+##'     verbosity level of error messages and warnings from the
+##'     underlying UNU.RAN library. It has no effect on messages
+##'     from the routines in this package.
+##'     Warnings are useful for analysing possible problems with the
+##'     selected combinations of distribution and method.
+##'     However, they can produce quite a lot of output if the
+##'     conditions of the method is appropriate for a distribution or
+##'     the distribution has properties like very heavy tails or
+##'     very high peaks.
+##'
+##'     The following levels can be set:
+##'     \describe{
+##'       \item{\code{"default"}:}{
+##'         same as \code{"warning"}.
+##'       }
+##'       \item{\code{"none"}:}{
+##'         all error messages and warnings are suppressed.
+##'       }
+##'       \item{\code{"error"}:}{
+##'         only show error messages.
+##'       }
+##'       \item{\code{"warning"}:}{
+##'         show error messages and some of the warnings.
+##'       }
+##'       \item{\code{"all"}:}{
+##'         show all error messages and warnings.
+##'       }
+##'     }
+##'   }
+##' }
+## 
+## --------------------------------------------------------------------------
+##'
+##' @author Josef Leydold \email{josef.leydold@@wu.ac.at}
+##'
+## --------------------------------------------------------------------------
+##' 
+##' @examples
+##'
+##' ## save current options
+##' oldval <- Runuran.options()
+##'
+##' ## show current options
+##' Runuran.options("error.level")
+##'
+##' ## suppress all UNU.RAN error messages and warnings
+##' Runuran.options(error.level="none")
+##'
+##' ## restore Runuran options
+##' Runuran.options(oldval)
+##'
+## --------------------------------------------------------------------------
+##
+##  Arguments:
+##
+##' @param \dots
+##'        A list may be given as the only argument, or any number of
+##'        arguments may be in the \code{name=value} form, a character
+##'        string for the name of a parameter, or no argument at all
+##'        may be given.
+##'
+## --------------------------------------------------------------------------
+##'
+##' @return
+##'
+##' \code{Runuran.options} returns a list with the updated values of the
+##' parameters. If the argument list is not empty, the returned list
+##' is invisible. If a single character string is given, then the
+##' value of the corresponding parameter is returned (or \code{NULL}
+##' if the parameter is not used).
+##'
 ### --------------------------------------------------------------------------
 ### @export
 ### --------------------------------------------------------------------------
